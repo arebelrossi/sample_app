e2d320c6eb520e0165a6193b3a54824fa1c0dec3
Dario <d.pedicini@gmail.com>
2012-04-01 20:23:02 +0200

    small fixes on user pages and tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,6 +13,13 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button "Create my account" }.not_to change(User, :count)
       end
+      describe "error messages" do
+        before { click_button "Create my account" }
+        
+        it { should have_selector('title', text: "Sign up") }
+        it { should have_css('div#error_explanation') }
+        it { should have_content("The form contains 6 errors") }
+      end
     end
 
     describe "with valid information" do
@@ -27,8 +34,14 @@ describe "User pages" do
           click_button "Create my account" 
         end.to change(User, :count).by(1)
       end
-    end
+      describe "after saving the user" do
+        before { click_button "Create my account" }
+        let(:user) { User.find_by_email('user@example.com') }
 
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+    end
   end
   describe "profile page" do
   	let(:user) { FactoryGirl.create(:user) }
